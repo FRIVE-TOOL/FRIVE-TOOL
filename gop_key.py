e402e22801b0c4349e952dcd4dba8bda8e160fc0
Bridget <140707104+FRIVE-TOOL@users.noreply.github.com>
2023-08-03 17:42:46 +0700

    Add files via upload

---
+++ gop_key.py
@@ -0,0 +1,3 @@
+import requests
+
+input('\033[35mTOOL ĐÃ UPDATE BẢN MỚI \n\033[1;34mTẢI TOOL TẠI LINK\033[1;37m: \033[1;33mhttps://drive.google.com/file/d/1n7Qtw1RsVcQj0OYsl2mf3BGdH7mm7-Hm/view?usp=sharing\nẤN ENTER ĐỂ EXIT')
